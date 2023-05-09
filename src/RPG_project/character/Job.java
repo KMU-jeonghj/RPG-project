7b2402f198ec1c8ff57eed67b01e9006290e7fdb
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2023-05-09 14:34:39 +0900

    new extended class from Job

---
+++ src/RPG_project/character/Job.java
@@ -0,0 +1,8 @@
+package RPG_project.character;
+
+public abstract class Job {
+
+    public abstract void work();
+
+    public abstract void skill();
+}
---
+++ src/RPG_project/character/Salary.java
@@ -0,0 +1,15 @@
+package RPG_project.character;
+
+public class Salary extends Job{
+
+
+    @Override
+    public void work() {
+
+    }
+
+    @Override
+    public void skill() {
+
+    }
+}
