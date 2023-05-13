e5c90daa5dc18cd1cd61c6ce35800e630f300d6b
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2023-05-13 14:24:01 +0900

    Salary work() 작성

--- src/RPG_project/character/NightGang.java
+++ src/RPG_project/character/NightGang.java
@@ -1,4 +1,10 @@
 package RPG_project.character;
 
 public class NightGang {
+
+    private String gangName;
+    private String gangRank;
+    private String[] rankArr = {"막내", "행동대장", "간부", "부두목", "두목"};
+
+    private int gangCnt, def, credibility;
 }
