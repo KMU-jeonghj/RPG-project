fc0da98ce09a91d529bce172b15234aff5bac7b9
KMU-jeonghj <66367591+KMU-jeonghj@users.noreply.github.com>
2023-05-28 03:04:24 +0900

    Item, Shop 인벤토리 동적배열구조 마무리

--- src/RPG_project/item/Inventory.java
+++ src/RPG_project/item/Inventory.java
@@ -2,11 +2,25 @@ package RPG_project.item;
 
 import RPG_project.character.*;
 
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class Inventory {
+    //동적 배열에 저장, 번호로 정렬해서 출력
+    private ArrayList<Item> itemList = new ArrayList<>();
 
 
 
 
+    public void addItem() {
+
+    }
+
+    public void printList() {
+
+    }
 
     public void useItem(Hero hero) {
 
